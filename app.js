ff5418c9ea02558dda5726c771d7f5af7d17f095
ymw0407 <yunminwo1211@gmail.com>
2023-05-25 00:35:10 +0900

    :hammer: fix : index.js -> app.js와 openapi.js로 분리 + yarn script에 forever과 nodemon으로 관리하는 script, app-start와 openapi-start 추가

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ import { connectDB } from "./MongoModule/mongoConnect.js";
 import { insertStationList, insertRoot, insertBlock } from "./MongoModule/insertDocument.js";
 import { updateStationList, updateRoot, updateBlock } from "./MongoModule/updateDocument.js";
 import { getStationInfo } from "./OpenAPI/stationInfoAPI.js";
-import { findStationList, findRootByID, findBlockByPlace, findBlockByID, makeBlockList } from "./MongoModule/findDocument.js";
-import { dataToPlace } from "./convertModule/dataToPlace.js";
+import { findStationList, findBlockByPlace } from "./MongoModule/findDocument.js";
 dotenv.config();
 
 const port = 8000;
@@ -159,7 +158,7 @@ app.get("/root/getRoot", async (req, res) => {
 });
 
 app.get("/block/getBlock", async (req, res) => {
-    const { collectionID, from, to , content} = req.body;
+    const { collectionID, from, to, content } = req.body;
     let response = {
         blockID: "",
         originContent: [],
@@ -228,184 +227,6 @@ app.patch("/root/patchRoot", async (req, res) => {
             console.log("------------------------------------");
             res.send(response);
         });
-});
-
-app.get("/openAPI/viewStation", async (req, res) => {
-    let { stationName } = req.query;
-
-    let response;
-
-    if (stationName != undefined) {
-        stationName.replace("역", "");
-        findStationList(mongoose, stationName)
-            .then((result) => {
-                if (result == null) {
-                    response = {
-                        returnValue: false,
-                        errCode: 100,
-                        errMsg: "Data was not collected or this station doesn't exist",
-                    };
-                    console.log("------------------------------------");
-                    console.log("[/openAPI/viewStation]");
-                    console.log(response);
-                    console.log("------------------------------------");
-                    res.status(404).send(response);
-                } else {
-                    response = {
-                        stationName: stationName + "역",
-                        lineList: result.lineInfo.sort(),
-                        rootList: result.rootInfo.map((root) => ({
-                            startAt: {
-                                line: root.startAt.line,
-                                next: root.startAt.next + "역",
-                            },
-                            endAt: {
-                                line: root.endAt.line,
-                                next: root.endAt.next + "역",
-                            },
-                        })),
-                        returnValue: true,
-                    };
-
-                    console.log("------------------------------------");
-                    console.log("[/openAPI/viewStation]");
-                    console.log(response);
-                    console.log("------------------------------------");
-                    res.status(200).send(response);
-                }
-            })
-            .catch((err) => {
-                response = {
-                    returnValue: false,
-                    errCode: 101,
-                    errMsg: "Server Error Occured",
-                };
-                console.log("------------------------------------");
-                console.log("[/openAPI/viewStation]");
-                console.log(response);
-                console.log(err);
-                console.log("------------------------------------");
-                res.status(500).send(response);
-            });
-    } else {
-        response = {
-            returnValue: false,
-            errCode: 102,
-            errMsg: "Incorrect query",
-        };
-        console.log("------------------------------------");
-        console.log("[/openAPI/viewStation]");
-        console.log(response);
-        console.log("------------------------------------");
-        res.status(404).send(response);
-    }
-});
-
-app.get("/openAPI/viewRoot", async (req, res) => {
-    let { stationName, start, end } = req.query;
-
-    let response = {
-        stationName: "",
-        root: {
-            start: {
-                line: "",
-                nextStation: "",
-            },
-            end: {
-                line: "",
-                nextStation: "",
-            },
-        },
-        blockList: [],
-        returnValue: true,
-    };
-    let collectionID;
-
-    /* stationName이랑 start랑 end 모든 데이터가 존재한다. */
-    if (stationName != undefined && start != undefined && end != undefined) {
-        stationName.replace("역", "");
-
-        /* findStationList를 통해서 collectionID와 rootID를 뽑아낸다. */
-        await findStationList(mongoose, stationName)
-            .then((result) => {
-                /* result에 데이터가 없다면, 해당 역이 존재하지 않는다는 의미 */
-                if (result == null) {
-                    response = {
-                        returnValue: false,
-                        errCode: 100,
-                        errMsg: "Data was not collected or this station doesn't exist",
-                    };
-                    console.log("------------------------------------");
-                    console.log("[/openAPI/viewRoot]");
-                    console.log(response);
-                    console.log("------------------------------------");
-                    res.status(404).send(response);
-                } 
-                else {
-                    collectionID = result.collectionID;
-                    response.stationName = `${result.stationName}역`;
-                    
-                    let index = -1;
-                    /* root의 갯수만큼 루트와 일치하는게 있는지 돌려본다 */
-                    for (let i = 0; i < result.rootInfo.length; i++) {
-                        /* root와 일치하는건 무조건 하나! */
-                        if (result.rootInfo[i].startAt.next == start && result.rootInfo[i].endAt.next == end) {
-                            index = i;
-                            break;
-                        }
-                    }
-                    if(index == -1){
-                        response = {
-                            returnValue: false,
-                            errCode: 103,
-                            errMsg: "Root data was not collected",
-                        };
-                        console.log("------------------------------------");
-                        console.log("[/openAPI/viewRoot]");
-                        console.log(response);
-                        console.log("------------------------------------");
-                        res.status(404).send(response);
-                    }
-                    else{
-                        makeBlockList(mongoose, collectionID, result.rootInfo[index].rootID, response)
-                            .then((response) => {
-                                console.log("------------------------------------");
-                                console.log("[/openAPI/viewRoot]");
-                                console.log(response);
-                                console.log("------------------------------------");
-                                res.status(500).send(response);
-                            })
-                    }
-                }
-                /* result에 데이터가 있다, 데이터 처리 */
-            })
-            .catch((err) => {
-                response = {
-                    returnValue: false,
-                    errCode: 101,
-                    errMsg: "Server Error Occured",
-                };
-                console.log("------------------------------------");
-                console.log("[/openAPI/viewRoot]");
-                console.log(response);
-                console.log(err);
-                console.log("------------------------------------");
-                res.status(500).send(response);
-            });
-        /* findStationList를 통해서 collectionID와 rootID를 뽑아낸다. */
-    } else {
-        response = {
-            returnValue: false,
-            errCode: 102,
-            errMsg: "Incorrect query",
-        };
-        console.log("------------------------------------");
-        console.log("[/openAPI/viewRoot]");
-        console.log(response);
-        console.log("------------------------------------");
-        res.status(404).send(response);
-    }
-    /* stationName이랑 start랑 end 중에 하나라도 없는 값이 있다면 */
 });
 
 app.listen(port, () => {
